6a951c4e90c3782002101ff9f6dd66d6cf3cc0b6
ckjerrgren <65974259+ckjerrgren@users.noreply.github.com>
2025-10-15 12:59:31 +0200

    Updated password name and explenation to code

--- .idea/app.js
+++ .idea/app.js
@@ -1,10 +1,10 @@
 
 
-// Hämta knappen
+// Hämtar knappen
 const button = document.getElementById("showMessageBtn");
 
-// Lösenord
-const correctPassword = "hej";
+// Lösenord - sätter ett löswenord
+const password = "hej";
 
 // Lägg till en händelse
 button.addEventListener("click", () => {
@@ -12,18 +12,21 @@ button.addEventListener("click", () => {
     // Fråga efter lösenord
     const userInput = prompt("Ange lösenord för att visa meddelandet:");
 
-    if (userInput === correctPassword) {
+    if (userInput === password) {
         console.log("Rätt lösenord");
 
-        // Ändra texten i meddelandet
+        // Ändra texten i meddelandet / första texten finns i html, men när rätt lösenord använts så
+        // ändras text till nya textContent 
         const message = document.getElementById("message");
         message.textContent = "Yay! Här är ditt meddelande:";
 
-        // Lägg till nytt element
+        // Lägg till nytt element för att visa upp nya texten.
         const section = document.getElementById("section");
         const newElement = document.createElement("p");
         newElement.textContent = "Du har låst upp det hemliga meddelandet! 🎉";
         section.appendChild(newElement);
+
+        // Om lösenordet är fel visas denna texten.
     } else {
         alert("Fel lösenord! Prova hej.");
     }
